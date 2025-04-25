92b65f600489af604b8d4f8a1ecb455a253c09c2
Maties90 <mmatejko62@gmail.com>
2025-04-25 21:40:18 +0200

    refactor: remove unused import for shared_preferences in log_in_screen.dart

--- ab_planner/lib/screens/log_in_screen.dart
+++ ab_planner/lib/screens/log_in_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ab_planner/screens/main_screen.dart';
 import 'package:ab_planner/screens/sign_up_screen.dart';
 import 'package:ab_planner/services/auth_service.dart';
